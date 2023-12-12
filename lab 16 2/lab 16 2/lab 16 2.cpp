aa036b4a7d9ec6b9b9eefaf366d2b919e7bec36d
Gritsenko Ruslan <ruslan17042006comp@gmail.com>
2023-12-12 15:20:59 +0200

    Лабораторна 16

---
+++ lab 16 2/lab 16 2/lab 16 2.cpp
@@ -0,0 +1,36 @@
+﻿#include <time.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    int matrix[5][5];
+
+    //Наповнюємо наш масив рандомними числами від 1 до 100
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            matrix[i][j] = rand() % 100 + 1;
+        }
+    }
+    // Виводимо цей масив
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            cout << matrix[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+    // Виводимо максимальні значення у рядках масиву
+    for (int i = 0; i < 5; ++i) {
+        int max = matrix[i][0];
+        for (int j = 1; j < 5; ++j) {
+            if (matrix[i][j] > max) {
+                max = matrix[i][j];
+            }
+        }
+        cout << "MAX " << i << ": " << max << " ";
+    }
+    return 0;
+}
+
---
+++ lab 16/lab 16/lab 16.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a[3][4] = { {2, 4, 5, 1}, {3, 6, 7, 2}, {1, 1, 9, 0} };
+    int b[4][3] = { {4, 4, 1}, {2, 5, 1}, {7, 7, 3}, {5, 9, 1} };
+    int transposed_b[3][4];
+    int result[3][4];
+
+    //Транспонуємо матрицю b
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            transposed_b[i][j] = b[j][i];
+        }
+    }
+
+    // Додаємо матрицю a до транспонової матриці b
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            result[i][j] = a[i][j] + transposed_b[i][j];
+        }
+    }
+
+    // Виводим результат
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            cout << result[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
