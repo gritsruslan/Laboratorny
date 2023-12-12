98e36f597f19aac5f8941ce44e3cd4fe7aa03d53
Gritsenko Ruslan <ruslan17042006comp@gmail.com>
2023-12-12 17:05:31 +0200

    commit 2

---
+++ lab 16 завдання 3/lab 16 завдання 3/lab 16 завдання 3.cpp
@@ -0,0 +1,39 @@
+﻿#include <time.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    int s[4][5];
+    int ArraySum[4];
+
+    // Наповнюємо наш масив рандомними числами від 1 до 100 включно
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 5; j++) {
+            s[i][j] = rand() % 100 + 1;
+        }
+    }
+    // Виводимо цей двумірний масив
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            cout << s[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    // Вираховуємо суми рядків та додаємо їх до вектору
+    for (int i = 0; i < 4; ++i) {
+        int sum = 0;
+        for (int j = 0; j < 5; ++j) {
+            if (s[i][j] > 0) {
+                sum += s[i][j];
+            }
+        }
+        ArraySum[i] = sum;
+    }
+    //Виводимо вектор з сумами рядків
+    for (int i = 0; i < 4; ++i) {
+        cout << "sum " << i << ": " << ArraySum[i] << " ";
+    }
+}
+
---
+++ lab 16 завдання 4/lab 16 завдання 4/lab 16 завдання 4.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int height, width;
+
+    cout << "Enter the height of the array: ";
+    cin >> height;
+    cout << "Enter the length of the array: ";
+    cin >> width;
+
+    vector<vector<int>> arr(height, vector<int>(width));
+
+    int k = 0;
+    // Наповнюємо вектор змійкою
+    for (int i = 0; i < height; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < width; j++) {
+                k++;
+                arr[i][j] = k;
+            }
+        }
+        else {
+            for (int j = width - 1; j >= 0; j--) {
+                k++;
+                arr[i][j] = k;
+            }
+        }
+    }
+    // Виводимо вектор
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
